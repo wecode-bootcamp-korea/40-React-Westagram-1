9c1eb9173d237c31519f8ddb942161bf99d627bb
minkyung_kwak <ellen10min@gmail.com>
2022-11-24 11:17:35 +0900

    40-minkyung branch 추가

--- src/pages/minkyung/Login/Login.js
+++ src/pages/minkyung/Login/Login.js
@@ -1,8 +1,55 @@
 import React from 'react';
 import './Login.scss';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-function MinkyungLogin() {
-  return <h1>Hello MinkyungLogin</h1>;
-}
+const Login = () => {
+  const navigate = useNavigate();
 
-export default MinkyungLogin;
+  const goToMain = () => {
+    navigate('/Main');
+  };
+
+  return (
+    <body className="wrap">
+      <main>
+        <div className="squareForm">
+          <div id="westagramLogo">
+            <h1>Westagram</h1>
+          </div>
+          <form id="loginForm">
+            <input
+              type="text"
+              id="user-id"
+              placeholder="&nbsp; 전화번호, 사용자 이름 또는 이메일"
+            />
+            <br />
+            <input
+              type="password"
+              id="user-password"
+              placeholder="&nbsp; 비밀번호"
+            />
+            <div className="validLogin">
+              <Link to="/Main">
+                <button
+                  type="button"
+                  id="login_btn"
+                  //disabled="disabled"
+                  onClick={goToMain}
+                >
+                  로그인
+                </button>
+              </Link>
+            </div>
+            <script type="text/javascript" src="js/login.js"></script>
+          </form>
+          <div className="forgotPassword">
+            <a href="../main/main.html">비밀번호를 잊으셨나요?</a>
+          </div>
+        </div>
+      </main>
+    </body>
+  );
+};
+
+export default Login;
--- src/pages/minkyung/Main/Main.js
+++ src/pages/minkyung/Main/Main.js
@@ -1,7 +1,193 @@
 import React from 'react';
+import './Main.scss';
 
-function MinkyungMain() {
-  return <h1>Hello MinkyungMain</h1>;
-}
+const Main = () => {
+  return (
+    <>
+      <div>
+        <nav className="navigation">
+          <div className="logo">
+            <img src="img/instagram.png" />
+            <span>westagram</span>
+          </div>
+          <div className="searchBar">
+            <input className="searchText" type="text" placeholder="검색" />
+          </div>
+          <div className="rightSideIcons">
+            <img src="img/explore.png" alt="Explore" />
+            <img src="img/heart.png" alt="Heart" />
+            <span>
+              <img src="img/profile.png" alt="Profile" />
+            </span>
+          </div>
+        </nav>
+        <div className="line"></div>
+      </div>
+      <main>
+        <div className="feeds">
+          <div className="article">
+            <div id="userIcon">
+              <img src="img/dog.jpg" alt="User Image" />
+              <div className="userName">cute_gyuri</div>
+              <div className="moreInfo">...</div>
+            </div>
+            <div id="articleImage">
+              <img src="img/article.jpeg" alt="Article Image" />
+            </div>
+            <div className="feedReactions">
+              <div className="feedRightIcons">
+                <img src="img/red_heart.png" alt="heart-icon" />
+                <img src="img/bubble-chat.png" alt="comment-icon" />
+                <img src="img/direct-instagram.png" alt="direct-message" />
+              </div>
+              <span className="saveIcon">
+                <img src="img/save-instagram.png" />
+              </span>
+            </div>
+            <div className="feedReactExp">
+              <div className="ReactUserId"></div>
+            </div>
+            <p className="feedArticle">
+              <strong>cute_gyuri</strong> 8000원이나 주고 산 밀크티. 곰돌이는
+              맛있어
+            </p>
+          </div>
+          <section className="user-comment">
+            <div className="commentLine">
+              <ul className="commentList">
+                <li>
+                  <span className="commentId">old_n_newyork</span>
+                  <span>곰돌이를 먹다니 잔인해!</span>
+                </li>
+              </ul>
+              <input
+                type="text"
+                className="commentSpace"
+                placeholder="댓글 달기..."
+              />
+              <button type="button" className="commentBtn">
+                게시
+              </button>
+            </div>
+          </section>
+        </div>
+        <div className="main-right">
+          <div className="personalProfile">
+            <div className="dropdown">
+              <span className="dropBtn">
+                <img src="img/dog.jpg" alt="profileImage" />
+              </span>
+              <ul class="dropdown-content">
+                <li href="#">
+                  <img src="img/user.png" />
+                </li>
+                <li href="#">
+                  <img src="img/save-instagram.png" />
+                </li>
+                <li href="#">
+                  <img src="img/settings.png" />
+                </li>
+                <li href="#">로그아웃</li>
+              </ul>
+            </div>
+            <div className="personalId">
+              cute_gyuri
+              <div id="personalName">maltipoo | 규리</div>
+            </div>
+          </div>
+          <div className="story">
+            <div className="storyBar">
+              스토리 <span>모두 보기</span>
+            </div>
+            <ul className="storyPeople">
+              <li className="storyContents">
+                <img alt="1st-profile" src="img/profile1.jpeg" />
+                <div className="ProfilesInfo">
+                  <strong>r_u_running</strong>
+                  <br />
+                  16분 전
+                </div>
+              </li>
+              <li className="storyContents">
+                <img alt="2nd-profile" src="img/profile2.jpeg" />
+                <div className="ProfilesInfo">
+                  <strong>cantdo_kendo</strong>
+                  <br />
+                  30분 전
+                </div>
+              </li>
+              <li className="storyContents">
+                <img alt="3rd-profile" src="img/profile3.jpeg" />
+                <div className="ProfilesInfo">
+                  <strong>cat_ssg_win</strong>
+                  <br />
+                  49분 전
+                </div>
+              </li>
+              <li className="storyContents">
+                <img alt="4th-profile" src="img/profile4.jpeg" />
+                <div className="ProfilesInfo">
+                  <strong>chocooooooo</strong>
+                  <br />
+                  1시간 전
+                </div>
+              </li>
+              <li className="storyContents">
+                <img alt="5th-profile" src="img/profile5.jpeg" />
+                <div className="ProfilesInfo">
+                  <strong>madoo_love</strong>
+                  <br />
+                  3시간 전
+                </div>
+              </li>
+              <li className="storyContents">
+                <img alt="6th-profile" src="img/profile6.jpeg" />
+                <div className="ProfilesInfo">
+                  <strong>old_n_newyork</strong>
+                  <br />
+                  20시간 전
+                </div>
+              </li>
+            </ul>
+            <div className="recommendation">
+              <div className="recommendationBar">
+                회원님을 위한 추천<span className="everyone">모두 보기</span>
+              </div>
+              <ul className="recommendationPeople">
+                <li className="storyContents">
+                  <img alt="7th-profile" src="img/profile7.jpeg" />
+                  <strong>tuna_sushi</strong>
+                  <br />
+                  <p className="recoExp1">r_u_running님 외...</p>
+                  {/* <span className="blueFollow">팔로우</span> */}
+                </li>
+                <li className="storyContents">
+                  <img alt="8th-profile" src="img/profile8.jpeg" />
+                  <strong>ducklingduckduck</strong>
+                  <br />
+                  <p className="recoExp2">old_n_newyork님...</p>
+                  {/* <span className="blueFollow">팔로우</span> */}
+                </li>
+                <li className="storyContents">
+                  <img alt="9th-profile" src="img/profile9.jpeg" />
+                  <strong>5am_cafe</strong>
+                  <br />
+                  <p className="recoExp3">chocooooooo님 외 ...</p>
+                  {/* <span className="blueFollow">팔로우</span> */}
+                </li>
+              </ul>
+            </div>
+            <div className="contract">
+              westagram 정보 지원 홍보 센터 API <br />
+              채용 정보 개인정보처리방침 약관 <br />
+              디렉터리 프로필 해시태그 언어 <br />
+              <br />© 2022 WESTAGRAM FROM WECODE
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default MinkyungMain;
+export default Main;
