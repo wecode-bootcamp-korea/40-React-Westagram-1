e7b0ca2a60518e04ac679e4200cb40a96123af9b
metamro <en160305@gmail.com>
2022-11-24 14:59:50 +0900

    1차 수정

--- src/pages/hakjin/Login/Login.js
+++ src/pages/hakjin/Login/Login.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import './Login.scss';
+import { useNavigate } from 'react-router-dom';
 
 function HakjinLogin() {
-  return <h1>Hello HakjinLogin</h1>;
+  const navigate = useNavigate();
+  return (
+    <div className="login">
+      <p id="logo">westagram</p>
+
+      <div className="login_tool">
+        <input
+          className="login_text"
+          id="email"
+          type="text"
+          onkeyup="emaillog()"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+        <input
+          className="login_text"
+          id="password"
+          type="password"
+          onkeyup="emaillog()"
+          placeholder="비밀번호"
+        />
+        <button
+          onClick={() => {
+            navigate('/main-hakjin');
+          }}
+          id="button"
+          /*disabled*/
+        >
+          로그인
+        </button>
+      </div>
+      <a id="miss_pass_word" href="https://www.instagram.com/">
+        비밀번호를 잊으셨나요?
+      </a>
+    </div>
+  );
 }
 
 export default HakjinLogin;
